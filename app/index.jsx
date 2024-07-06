699114d25552bc129c478518be6a7bdfa10e165f
ahmad <itxti909@gmail.com>
2024-07-06 20:41:22 +0300

    fonts and assets and layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,13 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl text-blue-500 font-pblack">App</Text>
       <StatusBar style="auto" />
-      <Link style={{ color: "blue" }} href={"/profile"}>
-        Go to profile
-      </Link>
+      <Link href={"/profile"}>Go to profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
