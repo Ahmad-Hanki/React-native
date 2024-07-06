845bc5ee90a33ea1b98c36ddca9c331614e8276b
ahmad <itxti909@gmail.com>
2024-07-06 20:08:35 +0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+      <StatusBar style="auto" />
+      <Link style={{ color: "blue" }} href={"/profile"}>
+        Go to profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
