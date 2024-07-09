2b4abbc055c956c8a7b25bcd3ee156b338fe315b
ahmad <itxti909@gmail.com>
2024-07-09 19:28:03 +0300

    animating the cards using Animatable.

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -12,12 +12,13 @@ import logoSmall from "../../assets/images/logo-small.png";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import Video from "../../components/Video";
 
 const Home = () => {
   const { data: posts, loading, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPost } = useAppwrite(getLatestPosts);
 
   const onRefresh = async () => {
     await refetch();
@@ -28,8 +29,9 @@ const Home = () => {
         data={posts} // array, same as map
         keyExtractor={(item) => item.$id} // extract the key
         renderItem={({ item }) => <Video video={item} />} // the thing i want to render
+       
         ListHeaderComponent={() => (
-          // flat list prop
+          // this one just renderes once!
 
           <View className="my-10 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
@@ -50,12 +52,12 @@ const Home = () => {
               </View>
             </View>
             <SearchInput />
-            {/* <View className="w-full flex-1 pt-5 pb-8">
+            <View className="w-full flex-1 pt-5 pb-8">
               <Text className="text-gray-100 text-lg font-pregular mt-3">
                 Latest videos
               </Text>
-              <Trending post={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
-            </View> */}
+              <Trending post={latestPost ?? []} />
+            </View>
           </View>
         )}
         // flat list prop
--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,14 +1,98 @@
-import { View, Text, FlatList } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import PPlay from "../assets/icons/play.png";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
+  return (
+    <Animatable.View
+      className="mr-5"
+      duration={500}
+      animation={activeItem == item.$id ? zoomIn : zoomOut}
+    >
+      {play && (
+        <>
+          <Text className="text-white">playing</Text>
+        </>
+      )}
+      {!play && (
+        <>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => setPlay(true)}
+            className="relative justify-center items-center "
+          >
+            <ImageBackground
+              resizeMode="cover"
+              className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+              source={{ uri: item.thumbnail }}
+            />
+
+            <Image
+              source={PPlay}
+              className="w-12 h-12 absolute"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ post }) => {
+  const [activeItem, setActiveItem] = useState(post[1]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
   return (
     <FlatList
       data={post}
-      keyExtractor={(item) => item.id}
+      keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white ">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
+
       horizontal
+      // makes you swipe
+      onViewableItemsChanged={viewableItemsChanged}
+      // gets the items anyway
+
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      // when do we start apply this itemVisiblePercentThreshold? after what?
+      // after 170 px of x
+      contentOffset={{ x: 170 }}
     />
   );
 };
